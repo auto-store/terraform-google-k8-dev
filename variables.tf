068a1ba8a93261616be6ed27e1064a0396370a74
Thomas Harris <tomh@Thomass-MacBook-Pro.local>
2020-02-18 18:52:34 +0000

    main.tf, vars.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "cluster_name" {
+  description = "name of GKE cluster"
+  type        = string
+}
+
+variable "location" {
+  description = "location of cluster"
+  type        = string
+}
+
+variable "pool_name" {
+  description = "node pool name"
+  type        = string
+}
+
+variable "type" {
+  description = "size of machine"
+  type        = string
+  default     = "n1-standard-1"
+}
+
+variable "node_count" {
+  description = "number of worker nodes"
+  type        = number
+}
+
